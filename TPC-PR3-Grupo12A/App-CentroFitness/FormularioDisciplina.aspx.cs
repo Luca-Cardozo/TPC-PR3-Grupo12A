d5152f9ae5ce66bf49845d2bd3d9141fbc40292a
Fio López Lezcano <fiorella.lopez@hotmail.com>
2026-06-04 21:58:46 -0300

    Carga automatica de datos al querer modificar/eliminar una disciplina en formularioDisciplina

--- TPC-PR3-Grupo12A/App-CentroFitness/FormularioDisciplina.aspx.cs
+++ TPC-PR3-Grupo12A/App-CentroFitness/FormularioDisciplina.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,51 @@ namespace App_CentroFitness
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!IsPostBack)
+            {
+
+                if (Request.QueryString["id"] != null)
+                {
+                    int idSeleccionado = int.Parse(Request.QueryString["id"]);
+
+                    try
+                    {
+
+                        List<Disciplina> lista;
+                        if (Session["listaDisciplinas"] != null)
+                        {
+                            lista = (List<Disciplina>)Session["listaDisciplinas"];
+                        }
+                        else
+                        {
+
+                            DisciplinaNegocio negocio = new DisciplinaNegocio();
+                            lista = negocio.listar();
+                        }
+
+                        Disciplina seleccionada = lista.Find(x => x.IdDisciplina == idSeleccionado);
+
+                        if (seleccionada != null)
+                        {
+
+                            txtIdDisciplina.Text = seleccionada.IdDisciplina.ToString();
+                            txtNombre.Text = seleccionada.Nombre;
+                            txtImagen.Text = seleccionada.Imagen;
+
+
+                            if (!string.IsNullOrEmpty(seleccionada.Imagen))
+                            {
+                                imgDisciplina.ImageUrl = seleccionada.Imagen;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Response.Write("<script>alert('Error al cargar la disciplina: " + ex.Message + "');</script>");
+                    }
+                }
+            }
         }
     }
 }
