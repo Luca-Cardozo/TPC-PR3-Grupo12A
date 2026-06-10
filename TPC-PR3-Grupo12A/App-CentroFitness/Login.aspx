666b432c2be90587474a39bcd6bc289a63e4b6d0
Luca Cardozo <lucacardozo99@gmail.com>
2026-06-10 19:46:24 -0300

    Agregado de funcionalidad al Login y UsuarioNegocio con método para login

--- TPC-PR3-Grupo12A/App-CentroFitness/Login.aspx
+++ TPC-PR3-Grupo12A/App-CentroFitness/Login.aspx
@@ -2,7 +2,7 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">    
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="container vh-100 d-flex justify-content-center align-items-center">
         <div class="card shadow p-4" style="width: 400px;">
 
@@ -19,11 +19,15 @@
             </div>
 
             <div class="d-grid gap-2">
-                <asp:Button Text="Ingresar" CssClass="btn btn-primary" ID="btnLogin" runat="server" />
+                <asp:Button Text="Ingresar" CssClass="btn btn-primary" ID="btnLogin" runat="server" OnClick="btnLogin_Click" />
 
                 <asp:Button ID="btnVolverHome" runat="server" Text="Volver a página principal"
                     OnClick="btnVolverHome_Click"
                     CssClass="btn btn-outline-secondary" />
+            </div>
+
+            <div class="d-grid gap-2">
+                <asp:Label ID="lblError" runat="server" CssClass="text-danger d-block mb-3 text-center" />
             </div>
 
         </div>
---
+++ TPC-PR3-Grupo12A/Negocio/UsuarioNegocio.cs
@@ -0,0 +1,83 @@
+﻿using Acceso_Datos;
+using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        public Usuario login(string email, string password)
+        {
+            Usuario usuario = null;
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT IdUsuario, Nombre, Apellido, Email, Password, DNI, " +
+                "Telefono, FechaNacimiento, Imagen, Rol, Observaciones, Activo FROM Usuarios " +
+                "WHERE Email = @Email AND Password = @Password AND Activo = 1"
+                );
+
+                datos.setearParametro("@Email", email);
+
+                datos.setearParametro("@Password", password);
+
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    Rol rol = (Rol)(int)datos.Lector["Rol"];
+
+                    if (rol == Rol.Alumno)
+                    {
+                        Alumno alumno = new Alumno();
+
+                        alumno.Observaciones = datos.Lector["Observaciones"] != DBNull.Value ? datos.Lector["Observaciones"].ToString() : "";
+
+                        usuario = alumno;
+                    }
+                    else
+                    {
+                        usuario = new Usuario();
+                    }
+
+                    usuario.IdUsuario = (int)datos.Lector["IdUsuario"];
+
+                    usuario.Nombre = datos.Lector["Nombre"].ToString();
+
+                    usuario.Apellido = datos.Lector["Apellido"].ToString();
+
+                    usuario.Email = datos.Lector["Email"].ToString();
+
+                    usuario.Password = datos.Lector["Password"].ToString();
+
+                    usuario.DNI = datos.Lector["DNI"].ToString();
+
+                    usuario.Telefono = datos.Lector["Telefono"].ToString();
+
+                    usuario.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
+
+                    usuario.Imagen = datos.Lector["Imagen"].ToString();
+
+                    usuario.Rol = rol;
+
+                    usuario.Activo = (bool)datos.Lector["Activo"];
+                }
+
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
